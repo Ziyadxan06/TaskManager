3baa10ba07c92db179482581804d93bf8b3d1a9c
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2025-05-06 11:46:54 +0400

    Integrate View Binding into the project

--- app/src/main/java/com/example/taskmanager/SignUpFragment.kt
+++ app/src/main/java/com/example/taskmanager/SignUpFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.taskmanager.databinding.FragmentSignInBinding
+import com.example.taskmanager.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return (binding.root)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
