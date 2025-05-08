0acb008cd3767acef6b6f164c3d2c435f3f783ca
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2025-05-08 12:15:07 +0400

    feat: create EditTaskFragment for updating existing tasks

---
+++ app/src/main/java/com/example/taskmanager/EditTaskFragment.kt
@@ -0,0 +1,25 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskmanager.databinding.AdminFragmentTaskListBinding
+import com.example.taskmanager.databinding.FragmentEditTaskBinding
+import com.example.taskmanager.databinding.FragmentTaskDetailsDialogBinding
+
+class EditTaskFragment : Fragment() {
+
+    private var _binding: FragmentEditTaskBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentEditTaskBinding.inflate(inflater, container, false)
+        return (binding.root)
+    }
+}
