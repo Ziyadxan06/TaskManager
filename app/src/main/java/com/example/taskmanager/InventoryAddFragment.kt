80050978f75b4493198ff2c2016f6ed6445e7a03
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2025-05-15 11:16:55 +0400

    feat(inventory): create InventoryAddFragment for adding new items

---
+++ app/src/main/java/com/example/taskmanager/InventoryAddFragment.kt
@@ -0,0 +1,28 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskmanager.databinding.FragmentInventoryAddBinding
+import com.example.taskmanager.databinding.FragmentInventoryListBinding
+
+class InventoryAddFragment : Fragment() {
+
+    private var _binding: FragmentInventoryAddBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentInventoryAddBinding.inflate(inflater, container, false)
+        return (binding.root)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
