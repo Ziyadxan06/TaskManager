325ffa7a2ff321885213a74c64262dece758a920
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2025-05-15 11:11:32 +0400

    feat(inventory): add FAB to AdminInventoryListFragment for item creation

--- app/src/main/java/com/example/taskmanager/AdminInventoryListFragment.kt
+++ app/src/main/java/com/example/taskmanager/AdminInventoryListFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.taskmanager.databinding.FragmentStaffTaskListBinding
 
 class AdminInventoryListFragment : Fragment() {
+
+    private var _binding: FragmentStaffTaskListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inventory_list, container, false)
+        _binding = FragmentStaffTaskListBinding.inflate(inflater, container, false)
+        return (binding.root)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
