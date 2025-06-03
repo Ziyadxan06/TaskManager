a2e5655009b8d221541f46f48cad1381f47105ba
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2025-06-03 14:58:26 +0400

    feat: Add new ArchiveFragment for displaying archived items

---
+++ app/src/main/java/com/example/taskmanager/ArchiveFragment.kt
@@ -0,0 +1,28 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskmanager.databinding.FragmentArchiveBinding
+import com.example.taskmanager.databinding.FragmentInventoryDetailsDialogBinding
+
+class ArchiveFragment : Fragment() {
+
+    private var _binding: FragmentArchiveBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentArchiveBinding.inflate(inflater, container, false)
+        return (binding.root)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
